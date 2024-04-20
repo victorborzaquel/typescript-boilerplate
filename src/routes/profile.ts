40e48a5204ce44e2720e6caafe5955bd4ee52095
Victor Borzaquel <victorborzaquel@outlook.com>
2024-04-20 03:03:13 -0300

    fix

--- src/routes/profile.ts
+++
@@ -1,7 +0,0 @@
-import {createRoute} from '@/lib/http';
-import {verifyJWT} from '@/middlewares/verify-jwt';
-
-export const profile = createRoute({
-  middlewares: [verifyJWT],
-  handler: async ({user}) => {},
-});
--- src/routes/verify.ts
+++ src/routes/verify.ts
@@ -1,28 +1,7 @@
-import {Status} from '@/enum/status';
-import {env} from '@/lib/env';
 import {createRoute} from '@/lib/http';
-import {ResponseError} from '@/lib/http/error';
-import jwt from 'jsonwebtoken';
+import {verifyJWT} from '@/middlewares/verify-jwt';
 
 export const verify = createRoute({
-  handler: async ({headers, response}) => {
-    const token = headers.authorization?.split(' ')?.[1];
-    if (!token) {
-      throw new ResponseError({
-        message: 'Not have token',
-        status: Status.UNAUTHORIZED,
-      });
-    }
-
-    try {
-      const payload = jwt.verify(token, env.JWT_SECRET);
-
-      return response(payload);
-    } catch (error) {
-      throw new ResponseError({
-        message: 'Invalid token',
-        status: Status.UNAUTHORIZED,
-      });
-    }
-  },
+  middlewares: [verifyJWT],
+  handler: async ({response, user}) => response(user),
 });
