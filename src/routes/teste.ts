162b7b2786252150f2655f0e63d484b9cfd4a2b6
Victor Borzaquel <victorborzaquel@outlook.com>
2024-04-29 23:55:19 -0300

    fix

--- src/routes/teste.ts
+++ src/routes/teste.ts
@@ -4,11 +4,8 @@ import {createSlugFromText} from '@/helpers/slug';
 import {createRoute} from '../lib/http';
 
 export const teste = createRoute({
-  handler: async ({response}) => {
-    return response(
-      createSlugFromText(
-        capitalize(interpolate('Hello, {{name}}! ola victor', {name: 'World'}))
-      )
-    );
-  },
+  handler: () =>
+    createSlugFromText(
+      capitalize(interpolate('Hello, {{name}}! ola victor', {name: 'World'}))
+    ),
 });
